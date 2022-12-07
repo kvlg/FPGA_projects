f8169b321bf68a3fa915a4a9dfd06af03104e88a
Kirill Kovaldzhi <118468605+kvlg@users.noreply.github.com>
2022-12-07 23:05:21 +0300

    Delete decoder_2x4_ternar.v

--- decoder_2x4_ternar.v
+++
@@ -1,9 +0,0 @@
-module decoder_2x4_ternar (input [1:0] a, output [3:0] b);
-
-  assign b = (a == 0) ? 4'b1110  :
-             (a == 1) ? 4'b1101  :
-             (a == 2) ? 4'b1011  :
-             (a == 3) ? 4'b0111  :
-                        4'bxxxx  ; 
-
-endmodule
